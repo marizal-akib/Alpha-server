ee51593105c55ba18569c17925e99aa9b08fc0ba
marizal-akib <marizalakib@gmail.com>
2023-11-29 13:44:21 +0000

    creat subscribe api

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const port = process.env.PORT || 5000;
 //middleware
 
 app.use(cors({
-    origin:[
-        "http://localhost:5173",
-    ],
-    credentials: true,
+  origin: [
+    "http://localhost:5173",
+  ],
+  credentials: true,
 }));
 app.use(express.json());
 
@@ -18,7 +18,7 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://marizalakib:pQZXjlafu5hcVbxp@cluster0.irissz7.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.irissz7.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -36,24 +36,48 @@ async function run() {
 
     const reviewCollection = client.db('alphaDB').collection('reviews')
     const photoCollection = client.db('alphaDB').collection('photos')
+    const postCollection = client.db('alphaDB').collection('posts')
+    const subCollection = client.db('alphaDB').collection('subs')
 
     // photo api
 
     app.get('/photos', async (req, res) => {
-        const page  = parseInt(req.query.page)
-        const size  = parseInt(req.query.size)
-          console.log(page, size);
-        const result = await photoCollection.find().skip(page*size).limit(size).toArray();
-        res.send(result);
+      const page = parseInt(req.query.page)
+      const size = parseInt(req.query.size)
+      console.log(page, size);
+      const result = await photoCollection.find().skip(page * size).limit(size).toArray();
+      res.send(result);
     })
 
-
+    // reviews api
     app.get('/reviews', async (req, res) => {
-     
-        const result = await reviewCollection.find().toArray();
-        res.send(result);
+
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
     })
 
+    // post api
+    app.get('/posts', async (req, res) => {
+      const size = parseInt(req.query.size)
+      const page = parseInt(req.query.page)
+      const result = await postCollection.find().sort({ vote: -1 }).skip(page * size).limit(size).toArray();
+      res.send(result);
+    })
+// newsletter api
+subCollection.createIndex({ email: 1 }, { unique: true });
+app.post('/sub', async (req, res) => {
+  const user = req.body;
+  try {
+    const result = await subCollection.insertOne(user);
+    res.send(result);
+  } catch (error) {
+    if (error.code === 11000) {
+      res.status(400).send('Duplicate entry found. Please provide a unique value.');
+    } else {
+      res.status(500).send('Internal server error');
+    }
+  }
+})
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -64,9 +88,9 @@ async function run() {
 }
 run().catch(console.dir);
 app.get('/', (req, res) => {
-    res.send('Alpha is running')
+  res.send('Alpha is running')
 })
 
 app.listen(port, () => {
-    console.log(`Bistro Alpha is working out on port ${port}`);
+  console.log(`Bistro Alpha is working out on port ${port}`);
 })
